058c914492f1bd73aacf8f45bb407586eb630df3
Walmir Neto <wfsneto@gmail.com>
2015-04-25 23:58:18 -0300

    add error to layout

--- resources/views/layouts/administration.blade.php
+++ resources/views/layouts/administration.blade.php
@@ -80,6 +80,27 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <strong>Oops!</strong> Ocorreu alguns erros.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
                 @yield('content')
             </div>
         </div>
