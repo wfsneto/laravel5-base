920d7d5226538a406067e92e7d3b80a252dbe05b
Walmir Neto <wfsneto@gmail.com>
2015-04-27 15:11:50 -0300

    adjustments

--- app/Models/Decorator/Base.php
+++ app/Models/Decorator/Base.php
@@ -9,77 +9,38 @@ trait Base
         return trans('modules.' . $this->name . '.' . str_singular($this->name));
     }
 
-    public function messages($type)
-    {
-        $translation = 'messages.' . $type;
-        $translation .= in_array($this->gender, [ 'f', 'm' ]) ? '.' . $this->gender : null;
-
-        return $this->name() . trans($translation);
-    }
-
     public function message($success, $message = null)
     {
         $this->success = $success;
 
         switch ($message) {
             case 'success':
-                $this->message = $this->messages_success();
+                $this->message = \Message::success($this->name, $this->gender);
                 break;
 
             case 'created':
-                $this->message = $this->messages_created();
+                $this->message = \Message::created($this->name, $this->gender);
                 break;
 
             case 'updated':
-                $this->message = $this->messages_updated();
+                $this->message = \Message::updated($this->name, $this->gender);
                 break;
 
             case 'removed':
-                $this->message = $this->messages_removed();
+                $this->message = \Message::removed($this->name, $this->gender);
                 break;
 
             case 'try_again':
-                $this->message = $this->messages_try_again();
+                $this->message = \Message::try_again($this->name, $this->gender);
                 break;
 
             case 'not_found':
-                $this->message = $this->messages_not_found();
+                $this->message = \Message::not_found($this->name, $this->gender);
                 break;
 
             default:
                 $this->message = $message;
                 break;
         }
-    }
-
-    # Aliases
-
-    public function messages_success()
-    {
-        return $this->messages('success');
-    }
-
-    public function messages_created()
-    {
-        return $this->messages('created');
-    }
-    public function messages_updated()
-    {
-        return $this->messages('updated');
-    }
-
-    public function messages_removed()
-    {
-        return $this->messages('removed');
-    }
-
-    public function messages_try_again()
-    {
-        return $this->messages('try_again');
-    }
-
-    public function messages_not_found()
-    {
-        return $this->messages('not_found');
     }
 }
