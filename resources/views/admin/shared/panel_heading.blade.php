db4f27db2d17467ae4ce1e5b8a68db276a53e5c4
Walmir Neto <wfsneto@gmail.com>
2015-05-18 19:53:38 -0300

    fix menu

--- resources/views/admin/shared/panel_heading.blade.php
+++ resources/views/admin/shared/panel_heading.blade.php
@@ -4,41 +4,31 @@ list($controller, $action) = explode('@', Route::getCurrentRoute()->getActionNam
 $params = explode('\\', $controller);
 $name = snake_case(str_replace('Controller', '', end($params))); ?>
 
-<div class="panel-heading">
-
 <div class="row">
-    <div class="col-md-12">
+    <div class="col s12">
         <i class="{{ trans($prefix . $name . '.fa_icon') }}"></i>
         {{ trans($prefix . $name . '.' . $name) }}
 
-        {{-- Split button --}}
-        <div class="btn-group">
-            <span class="btn btn-xs btn-default">
-                <i class="{{ trans('general.icon_' . $action) }}"></i>
-                {{ trans('general.' . $action) }}
-            </span>
-            <span class="btn btn-xs btn-default dropdown-toggle" data-toggle="dropdown">
-                <i class="{{ trans('general.icon_chevron_down') }}"></i>
-                <span class="sr-only"></span>
-            </span>
-            <ul id="actions-menu" class="dropdown-menu" role="menu">
-                @if($action != 'index')
-                    <li>
-                        <a href="{{ action('\\' . $controller . '@index') }}">
-                            <i class="fa fa-bars"></i> {{ trans('general.index') }}
-                        </a>
-                    </li>
-                @endif
-                @if($action != 'create')
-                    <li>
-                        <a href="{{ action('\\' . $controller . '@create') }}">
-                            <i class="fa fa-plus"></i> {{ trans('general.create') }}
-                        </a>
-                    </li>
-                @endif
-            </ul>
-        </div>
-    </div>
-</div>
+        <a class="dropdown-button btn blue" href="#!" data-activates="{{ $action }}-header-actions">
+            <i class="{{ trans('general.icon_' . $action) }}"></i>
+            {{ trans('general.' . $action) }}
+        </a>
 
+        <ul id="{{ $action }}-header-actions" class="dropdown-content">
+            @if($action != 'index')
+                <li>
+                    <a href="{{ action('\\' . $controller . '@index') }}">
+                        <i class="fa fa-bars"></i> {{ trans('general.index') }}
+                    </a>
+                </li>
+            @endif
+            @if($action != 'create')
+                <li>
+                    <a href="{{ action('\\' . $controller . '@create') }}">
+                        <i class="fa fa-plus"></i> {{ trans('general.create') }}
+                    </a>
+                </li>
+            @endif
+        </ul>
+    </div>
 </div>
