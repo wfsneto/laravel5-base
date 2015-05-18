fcf577714628465549eb87b18331609e6a9b6518
Walmir Neto <wfsneto@gmail.com>
2015-05-18 17:09:51 -0300

    adjustments navigation

--- resources/views/layouts/administration.blade.php
+++ resources/views/layouts/administration.blade.php
@@ -15,37 +15,71 @@
 @stop
 
 @section('nav')
-  <nav role="navigation">
-    <a class="brand-logo" href="{{ action('HomeController@index') }}">
-      {{ env('APP_NAME') }}
-    </a>
+<nav role="navigation">
+  <a class="brand-logo" href="{{ action('HomeController@index') }}">
+    {{ env('APP_NAME') }}
+  </a>
 
-    @include('admin/shared/navigation')
-  </nav>
+  <div class="nav-wrapper container">
+    @if(Auth::check())
+      <ul class="left hide-on-med-and-down">
+        @include('admin/shared/navigation')
+      </ul>
+    @endif
+
+    <ul class="right hide-on-med-and-down">
+      @if (Auth::guest())
+        <li><a href="{{ url('/auth/login') }}"><span class="fa fa-sign-in fa-fw"></span> {{ trans('modules.auth.sign_in') }}</a></li>
+        <li><a href="{{ url('/auth/register') }}"><span class="fa fa-user-plus fa-fw"></span> {{ trans('modules.auth.sign_up') }}</a></li>
+      @else
+        <li>
+          <a href="{{ url('/auth/logout') }}">
+            <span class="fa fa-sign-out fa-fw"></span>
+            {{ trans('modules.auth.sign_out') }}
+          </a>
+        </li>
+      @endif
+    </ul>
+
+    <a href="#" data-activates="mobile-menu" class="button-collapse">
+      <i class="mdi-navigation-menu"></i>
+    </a>
+    <ul class="side-nav" id="mobile-menu">
+      @include('admin/shared/navigation')
+      <li class="divider"></li>
+      <li>
+        <a href="{{ url('/auth/logout') }}">
+          <span class="fa fa-sign-out fa-fw"></span>
+          {{ trans('modules.auth.sign_out') }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
 @stop
 
 @section('container')
-  <div class="container">
-    @include('admin/shared/flash_messages')
+<div class="container">
+  @include('admin/shared/flash_messages')
 
-    <div class="row">
-      <div class="col-md-12">
-        @yield('content')
-      </div>
+  <div class="row">
+    <div class="col-md-12">
+      @yield('content')
     </div>
   </div>
+</div>
 @stop
 
 @section('footer')
-  <footer class="container">
-    <div class="row">
-      <div class="col-md-12 text-right">
-        <a href="//emmet.com.br" target="blank">
-          <img src="//emmet.com.br/img/logo.png" style="height: 21px;">
-        </a>
-      </div>
+<footer class="container">
+  <div class="row">
+    <div class="col-md-12 text-right">
+      <a href="//emmet.com.br" target="blank">
+        <img src="//emmet.com.br/img/logo.png" style="height: 21px;">
+      </a>
     </div>
-  </footer>
+  </div>
+</footer>
 @stop
 
 @section('scripts')
